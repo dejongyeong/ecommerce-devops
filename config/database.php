a420643aa520fda510cdc6313628096be843fe58
De Jong <dejong.re2@gmail.com>
2019-03-30 00:40:16 +0000

    change testing database to sqlite

--- config/database.php
+++ config/database.php
@@ -67,19 +67,6 @@ return [
             'sslmode' => 'prefer',
         ],
 
-        'pgsql_test' => [
-            'driver' => 'pgsql',
-            'host' => env('DB_TEST_HOST', '127.0.0.1'),
-            'port' => env('DB_TEST_PORT', '5432'),
-            'database' => env('DB_TEST_DATABASE', 'forge'),
-            'username' => env('DB_TEST_USERNAME', 'forge'),
-            'password' => env('DB_TEST_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
-            'sslmode' => 'prefer',
-        ],
-
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'host' => env('DB_HOST', 'localhost'),

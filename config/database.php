4e358d73ab491ac77d47473c8f9a4699255720d2
De Jong <dejong.re2@gmail.com>
2019-03-31 03:45:10 +0100

    heroku staging phase deployment

--- config/database.php
+++ config/database.php
@@ -1,5 +1,7 @@
 <?php
 
+$heroku_db_url = parse_url(env('DATABASE_URL', "postgres://forge:forge@localhost:5432/forge"));
+
 return [
 
     /*
@@ -65,6 +67,17 @@ return [
             'prefix' => '',
             'schema' => 'public',
             'sslmode' => 'prefer',
+        ],
+
+        'pg-heroku' => [
+            'driver'   => 'pgsql',
+            'host'     => $heroku_db_url['host'],
+            'database' => substr($heroku_db_url['path'], 1),
+            'username' => $heroku_db_url['user'],
+            'password' => $heroku_db_url['pass'],
+            'charset'  => 'utf8',
+            'prefix'   => '',
+            'schema'   => 'public',
         ],
 
         'sqlsrv' => [
